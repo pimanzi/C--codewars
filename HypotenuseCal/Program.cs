0f9e792525d91dab96e6e9ac0c263ede7ecb7a66
pimanzi <imanzikabisa@gmail.com>
2026-04-09 12:28:04 +0200

    feat: adding first project in c sharp about calculating hypotenuse of the triangle

---
+++ HypotenuseCal/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace  HypotenuseCal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Hypotenuse Calculator");
+            Console.Write("Enter the length of first side of triangle: ");
+            int length = int.Parse(Console.ReadLine());
+            Console.Write("Enter the length of second side of triangle: ");
+            int length2 = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The length of hypotenuse is {Math.Sqrt(Math.Pow(length, 2) + Math.Pow(length2, 2))}");
+
+        }
+    }
+    
+};
+
