2efb90817dafc48883a2bce2af5415c3005fa0cd
pimanzi <imanzikabisa@gmail.com>
2026-04-10 09:30:52 +0200

    feat: added a small application for guessing a number

---
+++ GuessGame/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace GuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Guess Number, Guess a number between 1 and 10");
+            Random rand = new Random();
+            int number = rand.Next(1,11);
+            Console.Write("Guess: ");
+            bool checkInput = int.TryParse(Console.ReadLine(), out int userResult);
+            
+            while (userResult !=number)
+            {
+                if (!checkInput)
+                {
+                    Console.WriteLine("Please enter a number between 1 and 10: ");
+                }
+                else if (userResult < number)
+                {
+                    Console.WriteLine("The number is below the correct number");
+                }
+                else
+                {
+                    Console.WriteLine("The number is above the correct number");
+                }
+                Console.Write("Guess: ");
+                checkInput = int.TryParse(Console.ReadLine(), out userResult);
+            }
+            
+            Console.WriteLine($"Congratulations! the number is {userResult}");
+            
+            
+
+        }
+    }
+}
+
